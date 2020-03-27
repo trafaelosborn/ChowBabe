f06856f179ccb46632b85ae74ab2284992e5bbe1
Taylor Osborn <osborn.taylor@gmail.com>
2020-03-26 21:02:50 -0700

    Added Card component and more styling in Search component

--- Client/src/Components/Search/Search.js
+++ Client/src/Components/Search/Search.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
-
+import { Container } from '@material-ui/core';
+import Card from '../Card/Card'
 
 export default function Search() {
     return (
         <div>
             <Navbar />
+            <Container>
+                <Card />
+            </Container>
         </div>
     )
 }
