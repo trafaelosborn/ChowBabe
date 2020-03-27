0fa7666b7dfff63ed324659a441890ded57870ef
Taylor Osborn <osborn.taylor@gmail.com>
2020-03-26 19:37:04 -0700

    Added switch routes and Signup page

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
-import Signin from './Components/Signin/Signin'
+import Signin from './Components/Signin/Signin';
+import Signup from './Components/Signup/Signup'
 
 function App() {
   return (
-    <Signin />
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path = "/" component = {Signin} />
+          <Route exact path = "/signup" component = {Signup}/>
+        </Switch>
+      </div>
+     </Router>
   );
 }
 
