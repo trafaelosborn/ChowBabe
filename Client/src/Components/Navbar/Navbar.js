e9a8d4d4d948377a3af13d3dd49704767f3ba292
Taylor Osborn <osborn.taylor@gmail.com>
2020-03-27 17:25:36 -0700

    Corrected repeating search function error

--- Client/src/Components/Navbar/Navbar.js
+++ Client/src/Components/Navbar/Navbar.js
@@ -68,16 +68,18 @@ const useStyles = makeStyles(theme => ({
 }));
 
 
+const handleSearch = function(event) {
+  event.preventDefault();
+   axios.get('https://api.edamam.com/search?q=broccoli&app_id=867a731f&app_key=8d8fa59906758f991bd7c52d34c5621f')
+ .then((response) => {
+   console.log(response)
+ })
+ }
 
 export default function SearchAppBar() {
   const classes = useStyles();
 
- const handleSearch = function() {
-    axios.get('https://api.edamam.com/search?q=broccoli&app_id=867a731f&app_key=8d8fa59906758f991bd7c52d34c5621f')
-  .then((response) => {
-    console.log(response)
-  })
-  }
+
   
 
   return (
@@ -96,7 +98,7 @@ export default function SearchAppBar() {
             Recipes
           </Typography>
           <div>
-              <Button onClick={handleSearch()}>Find Recipes!</Button>
+          <button onClick={ (event)=>{ handleSearch(event); }}>Get Recipes! </button>
             </div>
           <div className={classes.search}>
             <div className={classes.searchIcon}>
