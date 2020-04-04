70afd7137c95cd41d5c932cb5c70d6bff3e1a145
Dan Dismuke <dandismuke@gmail.com>
2020-04-04 12:46:08 -0700

    resolve merge conflict

--- Client/src/Components/ProfileGrid/ProfileGrid.js
+++ Client/src/Components/ProfileGrid/ProfileGrid.js
@@ -11,16 +11,8 @@ const gridStyle = {
 export default function ProfileGrid(props) {
 
 	const renderProfileGrid = () => {
- poplate-profile-data
 		return (<Grid container spacing={4} >
 			{props.recipes.map((item, index) => {
-
-		console.log('renderprofilegrid myRecipes:')
-		console.log(myRecipes)
-		return (<div>
-			<Grid container spacing={4}>
-			{myRecipes.map((item, index) => {
-master
 				return <Grid items >
 					<ProfileCard 
 						recipeName={item.recipeName}
@@ -28,15 +20,8 @@ master
 						dietLabels={item.dietLabels}
 					/>
 				</Grid>
-				
-			})} 
-poplate-profile-data
+			})}
 			 </Grid>)
-
-			</Grid>
-			</div>)
-		
-master
 	}
 
     return (
