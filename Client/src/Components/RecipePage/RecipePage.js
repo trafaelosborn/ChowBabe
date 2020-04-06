574b3eae0de0887c2927f1b0831234eafc7db1a9
Taylor Osborn <osborn.taylor@gmail.com>
2020-04-05 20:30:02 -0700

    Added tabs to recipe page

--- Client/src/Components/RecipePage/RecipePage.js
+++ Client/src/Components/RecipePage/RecipePage.js
@@ -3,11 +3,12 @@ import Navbar from '../Navbar/Navbar'
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import RecipeForm from '../RecipeForm/RecipeForm'
-
+import RecipeTabs from '../RecipeTabs/RecipeTabs'
 export default function RecipePage() {
     return (
         <div>
             <Navbar />
+            <RecipeTabs />
         </div>
     )
 }
