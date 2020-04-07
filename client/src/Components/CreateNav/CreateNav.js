e376133d8e3662edbb2f9a655f0acda82790f9aa
Taylor Osborn <osborn.taylor@gmail.com>
2020-04-07 16:26:39 -0700

    Updated Navbar

---
+++ client/src/Components/CreateNav/CreateNav.js
@@ -0,0 +1,111 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import Button from '@material-ui/core/Button';
+import axios from "axios"
+import Link from '@material-ui/core/Link';
+import API from '../../Utils/api'
+
+const useStyles = makeStyles(theme => ({
+  
+  root: {
+    flexGrow: 1,
+
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
+
+export default function SearchAppBar({search, handleSearch, handleInput}) {
+  	const classes = useStyles();
+  	const { id } = useParams();
+
+	return (
+    	<div className={classes.root}>
+      		<AppBar position="fixed">
+        		<Toolbar>
+					<IconButton
+						edge="start"
+						className={classes.menuButton}
+						color="inherit"
+						aria-label="open drawer"
+					>
+						<MenuIcon />
+					</IconButton>
+					<Typography className={classes.title} variant="h6" noWrap>
+						Recipes
+					</Typography>
+					<div>
+						<Typography className={classes.root}>
+						</Typography>
+					</div>
+					<div>
+						<Button href={"/createrecipe/" + id}> Create a Recipe </Button>
+						<Button href={"/profile/" + id}> My Profile </Button>
+						<Button href={"/search/" + id}> Find Recipes! </Button>
+					</div>
+					<div className={classes.search}>
+						<div className={classes.searchIcon}>
+							<SearchIcon />
+						</div>
+						
+					</div>
+        		</Toolbar>
+      		</AppBar>
+		</div>
+	);
+}
--- client/src/Components/CreateRecipe/CreateRecipe.js
+++ client/src/Components/CreateRecipe/CreateRecipe.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from '../Navbar/Navbar'
+import CreateNav from '../CreateNav/CreateNav'
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import RecipeForm from '../RecipeForm/RecipeForm'
@@ -7,7 +7,7 @@ import RecipeForm from '../RecipeForm/RecipeForm'
 export default function CreateRecipe() {
     return (
         <div>
-            <Navbar />
+            <CreateNav />
             <RecipeForm />
         </div>
     )
