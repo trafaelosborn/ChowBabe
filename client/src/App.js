1d848ef3ecb25b147159cef451169e46b08210ce
Taylor Osborn <osborn.taylor@gmail.com>
2020-04-12 20:55:01 -0700

    installed material UI icons

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import Signin from "./Components/Signin/Signin";
+// import Signin from "./Components/Signin/Signin";
 import Signup from "./Components/Signup/Signup";
 import Search from "./Components/Search/Search";
 import Profile from "./Components/Profile/Profile";
@@ -13,7 +13,7 @@ function App() {
 		<Router>
 			<div>
 				<Switch>
-					<Route exact path="/" component={Signin} />
+					{/* <Route exact path="/" component={Signin} /> */}
 					{/* <Route exact path="/signup" component={Signup} />
 					<Route exact path="/search" component={Search} />
 					<Route exact path="/profile" component={Profile} />
