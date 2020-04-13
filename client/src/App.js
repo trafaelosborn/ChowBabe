8370f8f6d60d7c3c70fd446c11b7e06c88b9b970
Taylor Osborn <osborn.taylor@gmail.com>
2020-04-12 20:52:18 -0700

    installed material UI icons

--- client/src/App.js
+++ client/src/App.js
@@ -7,17 +7,19 @@ import Search from "./Components/Search/Search";
 import Profile from "./Components/Profile/Profile";
 import RecipePage from "./Components/RecipePage/RecipePage";
 import CreateRecipe from "./Components/CreateRecipe/CreateRecipe";
+import Test from "./Components/Test/index"
 function App() {
 	return (
 		<Router>
 			<div>
 				<Switch>
 					<Route exact path="/" component={Signin} />
-					<Route exact path="/signup" component={Signup} />
+					{/* <Route exact path="/signup" component={Signup} />
 					<Route exact path="/search" component={Search} />
 					<Route exact path="/profile" component={Profile} />
 					<Route exact path="/RecipePage/:id" component={RecipePage} />
-					<Route exact path="/createrecipe" component={CreateRecipe} />
+					<Route exact path="/createrecipe" component={CreateRecipe} />  */}
+					<Route exact path="/test" component={Test} />
 				</Switch>
 			</div>
 		</Router>
